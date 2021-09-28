d6c04f290424f005e8e3cf4020092632f1f17d1c
Prem Mhatre <83350182+Prem-Mhatre@users.noreply.github.com>
2021-09-28 18:36:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -30,3 +30,40 @@ function speak(){
     utter_this = new SpeechSynthesisUtterance(speak_data1 + speak_data2);
     syth.speak(utter_this);
 }
+
+function check(){
+    img = document.getElementById("captured_img");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, result){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(result);
+        document.getElementById("result_name1").innerHTML = result[0].label;
+        document.getElementById("result_name2").innerHTML = result[1].label;
+        prediction1 = result[0].label;
+        prediction2 = result[1].label;
+        speak();
+        if(prediction1 == "amazing"){
+            document.getElementById("emoji1").innerHTML = "&#128076;";
+        }
+        if(prediction1 == "best"){
+            document.getElementById("emoji1").innerHTML = "&#128077;"
+        }
+        if(prediction1 == "victory"){
+            document.getElementById("emoji1").innerHTML = "&#9996;";
+        }
+        if(prediction2 == "amazing"){
+            document.getElementById("emoji2").innerHTML = "&#128076;";
+        }
+        if(prediction2 == "best"){
+            document.getElementById("emoji2").innerHTML = "&#128077;"
+        }
+        if(prediction2 == "victory"){
+            document.getElementById("emoji2").innerHTML = "&#9996;";
+        }
+    }
+}
